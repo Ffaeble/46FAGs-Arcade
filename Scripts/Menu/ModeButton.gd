c94764351bfb0ea1a75540a1d12762f455ecde31
MuffinMan <kilianguntermann@gmx.de>
2024-10-22 13:44:13 +0200

    Fml working on changing the Scene, might continue later
    Mode button now switches between "Roguelike" and "Roguelite".
    Start button didn't change running it.
    options Button throws error and crashes everything, trying to add second seen for options menu.

--- Scripts/Menu/ModeButton.gd
+++ Scripts/Menu/ModeButton.gd
@@ -4,7 +4,7 @@ func _on_toggled(toggled_on: bool) -> void:
 	GlobConf.mode = toggled_on
 	if toggled_on:
 		print("is true")
-		print(GlobConf.mode)
+		text = "Roguelike"
 	else:
 		print("is false")
-		print(GlobConf.mode)
+		text = "Roguelite"
